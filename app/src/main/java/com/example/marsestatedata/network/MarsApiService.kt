e22bb0b64d82083a91f4635d97dc024b3d287015
Oles Malysh <olesmalish37@gmail.com>
2024-01-23 11:52:59 +0100

    Moshi added

--- app/src/main/java/com/example/marsestatedata/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsestatedata/network/MarsApiService.kt
@@ -1,25 +1,35 @@
 package com.example.marsestatedata.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 /**
- This class it`s an API class where we describe how retrofit will fetch data from web-service.
- In the end retrofit return a data in a view like string or what we want */
+This class it`s an API class where we describe how retrofit will fetch data from web-service.
+In the end retrofit return a data in a view like string or what we want */
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
-private val retrofit = Retrofit.Builder().addConverterFactory(ScalarsConverterFactory.create()).baseUrl(BASE_URL).build()
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
-interface MarsApiService{
- @GET("realestate")
- fun getProperties(): Call<String>
+private val retrofit = Retrofit.Builder()
+    /*.addConverterFactory(ScalarsConverterFactory.create()) */// Converter for string
+    .addConverterFactory(MoshiConverterFactory.create(moshi)) // Converter JSON response from server to Kotlin object
+    .baseUrl(BASE_URL).build()
+
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties(): Call<List<MarsProperty>> // Do request to server
 }
 
-object MarsApi{
- val retrofitService: MarsApiService by lazy {
-  retrofit.create(MarsApiService::class.java)
- }
+object MarsApi {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
 }
--- app/src/main/java/com/example/marsestatedata/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/marsestatedata/overview/OverviewViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.marsestatedata.network.MarsApi
+import com.example.marsestatedata.network.MarsProperty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -24,12 +25,12 @@ class OverviewViewModel() : ViewModel() {
     private fun getMarsRealEstateProperties() {
         _response.value = "Set the Mars API Response here!"
 
-        MarsApi.retrofitService.getProperties().enqueue(object: Callback<String>{
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                _response.value = response.body()
+        MarsApi.retrofitService.getProperties().enqueue(object: Callback<List<MarsProperty>> {
+            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+                _response.value = "Success ${response.body()?.size} Mars properties retrieved"
             }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
                 _response.value = "Failure: ${t.message}"
             }
 
